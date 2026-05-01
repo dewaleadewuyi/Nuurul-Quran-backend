1f637b10a821e272883daf06a0495c1719cf15f8
dewaleadewuyi <95146590+dewaleadewuyi@users.noreply.github.com>
2026-05-01 16:45:21 +0100

    Fixed CORS to allow Vercel frontend

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['https://nuurul-quran-institute.vercel.app', 'http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
